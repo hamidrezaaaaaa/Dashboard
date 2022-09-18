78688ba9d1421155d590cca51fb40e4a1f27913a
Hamidreza <79729476+hamidrezaaaaaa@users.noreply.github.com>
2022-09-18 22:16:50 +0430

    Add contact form in account component.

--- src/App.js
+++ src/App.js
@@ -4,31 +4,44 @@ import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/styles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from "./routes";
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import styled from "styled-components";
+
+const PageRoot = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: ${(props) => (!props.matches ? `70px` : `0`)};
+`;
 
 function App() {
   const theme = createTheme();
   const themeRoot = useTheme();
-  const matches = useMediaQuery(themeRoot.breakpoints.up('sm'));
+  const matches = useMediaQuery(themeRoot.breakpoints.up("sm"));
   return (
-    <ThemeProvider theme={theme} >
-      <div  style={{display:'flex'}}>
-      <Router>
-        <Navigation />
-        <div style={!matches ? {paddingTop:'70px'} : {paddingTop:'0'}}>
-        <Routes>
-          {routes.map((route, index) => {
-            return (
-              <Route exact key={index} path={route.path} element={<route.component/>}>
-              </Route>
-            );
-          })}
-        </Routes>
-        </div>
-      </Router>
+    <ThemeProvider theme={theme}>
+      <div style={{ display: "flex" }}>
+        <Router>
+          <Navigation />
+          <PageRoot matches={matches}>
+            <Routes>
+              {routes.map((route, index) => {
+                return (
+                  <Route
+                    exact
+                    key={index}
+                    path={route.path}
+                    element={<route.component />}
+                  ></Route>
+                );
+              })}
+            </Routes>
+          </PageRoot>
+        </Router>
       </div>
-     </ThemeProvider>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,94 @@
-import React from "react";
+import React ,{useState} from "react";
+import styled from "styled-components";
+import { Card, CardHeader, CardContent, Grid, TextField ,FormControl,InputLabel,MenuItem,Button} from "@mui/material";
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-const Account =() =>{
-    return(
-        <div>account</div>
-    )
-}
 
-export default Account
+const CustomTextField = styled(TextField)`
+  label {
+    left: auto;
+    right: auto;
+    transform-origin: top right;
+    text-align: right;
+  }
+`;
+
+const Account = () => {
+
+  const [sex, setSex] = useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSex(event.target.value );
+  };
+  return (
+    <Card dir="rtl" sx={{ width: "80%" }}>
+      <CardHeader title="مشخصات فردی" />
+      <CardContent>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            <CustomTextField
+              label="نام"
+              variant="standard"
+              dir="rtl"
+              fullWidth
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <CustomTextField
+              label="نام خانوادگی"
+              variant="standard"
+              dir="rtl"
+              fullWidth
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <CustomTextField
+              label=" شماره تلفن"
+              variant="standard"
+              dir="rtl"
+              fullWidth
+              type="tel"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">جنسیت</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={sex}
+                label="جنسیت"
+                onChange={handleChange}
+              >
+                <MenuItem value={1}>مرد</MenuItem>
+                <MenuItem value={2}>زن</MenuItem>
+                <MenuItem value={3}>عدم تمایل به انتخاب</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12}>
+            <CustomTextField
+              label="ایمیل"
+              variant="standard"
+              dir="rtl"
+              fullWidth
+              type="email"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+          <Button fullWidth variant="contained">ثبت</Button>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Account;
